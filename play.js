1d7df61643c72845802ede41bc90533919768257
petrero <petrero@gmail.com>
2017-05-16 21:59:25 +0300

    Copy code from branch 9

---
+++ play.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const aGreatNumber = 10;
+const aGreatObject = { withGreatKeys: true };
+
+aGreatObject.withGreatKeys = false;
+
+if (true){
+  //aGreatNumber = 42;
+}
+
+setTimeout(() => {
+  console.log(aGreatNumber);
+  console.log(aGreatObject);
+}, 1000);
+
+console.log('waiting...');
+
