9716028af55f0ed3cd87353f1b8962c8ce5ed5a6
petrero <petrero@gmail.com>
2017-05-16 20:13:22 +0300

    Optional Args
    Ready for one more cool thing? This one is easy. Suppose we have a new method, not calculateTotalWeight(), but getTotalWeightString(). Use the new shorthand syntax and return this.calculateTotalWeight() and append 'pounds' to it.
    Perfect! Then above, in updateTotalWeightLifted(), instead of calling calculateTotalWeight() and passing that to .html(), pass getTotalWeightString().
    
    Ok, nothing too crazy so far: when we refresh, at the bottom, yep, 'pounds'.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -41,7 +41,7 @@
 
     updateTotalWeightLifted() {
       this.$wrapper.find('.js-total-weight').html(
-        this.helper.calculateTotalWeight()
+        this.helper.getTotalWeightString()
       );
     },
 
@@ -183,6 +183,10 @@
       });
 
       return totalWeight;
+    },
+    
+    getTotalWeightString() {
+      return this.calculateTotalWeight() + ' lbs';
     }
   });
 })(window, jQuery, Routing, swal);
