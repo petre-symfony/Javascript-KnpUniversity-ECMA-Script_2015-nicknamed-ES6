b96b96b5597ea043f4d04f627a7e066405e73155
petrero <petrero@gmail.com>
2017-05-17 18:36:08 +0300

    But of course, there are a lot of yummy things in the world, and since I'm from the US, my yummyThings should probably have a cheeseburger. What will happen now? The array has 4 things, but we only have 3 arguments? Let's find out! Run the script!
    It's the same result! Thanks to the spread operator, pizza is passed as the first argument, then gelato, sushi and finally, cheeseburger is passed as the 4th argument. Since the function doesn't have a 4th argument, it's just ignored!

--- play.js
+++ play.js
@@ -4,6 +4,6 @@ let printThreeThings = function(thing1, thing2, thing3){
   console.log(thing1, thing2, thing3);
 }
 
-let yummyThings = ['pizza', 'gelato', 'sushi'];
+let yummyThings = ['pizza', 'gelato', 'sushi', 'cheesburger'];
 
 printThreeThings(...yummyThings);
