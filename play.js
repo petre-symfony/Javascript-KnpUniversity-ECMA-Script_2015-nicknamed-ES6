2a8cc42935edaf4c9583b1735619d5be08f6f7cc
petrero <petrero@gmail.com>
2017-05-17 22:05:49 +0300

    Copy code from branch 16

---
+++ play.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let foods = new WeakMap();
+foods.set(['italian'], 'gelato');
+foods.set(['mexican'], 'torta');
+foods.set(['canadian'], 'poutine');
+
+let southernUSStates = ['Tennessee', 'Kentucky', 'Texas'];
+foods.set(southernUSStates, 'hot chicken');
+southernUSStates = null;
+
+console.log(
+  foods.get(['italian']),
+  foods.get(southernUSStates),
+  foods.size
+);
