a0f73ea748ab681c9f17133f46af6c12830a7ff0
petrero <petrero@gmail.com>
2017-05-17 19:43:40 +0300

    Remove the upper function and put everything back to normal.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -206,13 +206,8 @@
     }
   }
   
-  function upper(template, ...expressions) {
-    return template.reduce((accumulator, part, i) => {
-      return accumulator + (expressions[i - 1].toUpperCase ? expressions[i - 1].toUpperCase() : expressions[i - 1]) + part
-    })
-  }
   
-  const rowTemplate = (repLog) => upper`
+  const rowTemplate = (repLog) => `
     <tr data-weight="${repLog.totalWeightLifted}">
       <td>${repLog.itemLabel}</td>
       <td>${repLog.reps}</td>
