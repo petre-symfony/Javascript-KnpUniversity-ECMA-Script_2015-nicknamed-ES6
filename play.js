d43fa19989a006f39557828fecdbfd3e9b0d7669
petrero <petrero@gmail.com>
2017-05-17 21:02:32 +0300

    WeakMap has an extra requirement: its keys must be objects.
    Turn each key into an array, which is an object. At the bottom, use foods.get() and pass it the italian array.
    
    Now when I run it, it works fine. Wait, or, does it?
    
    Two interesting things: this prints undefined, hot chicken, undefined. First, even though the ['italian'] array in get() is equal to the ['italian'] array used in set, they are not the same object in memory. These are two distinct objects, so it looks like a different key to WeakMap. That's why it prints undefined.
    
    Second, with WeakMap, you can't call foods.size. That's just not something that works with WeakMap.

--- play.js
+++ play.js
@@ -1,15 +1,15 @@
 'use strict';
 
 let foods = new WeakMap();
-foods.set('italian', 'gelato');
-foods.set('mexican', 'torta');
-foods.set('canadian', 'poutine');
+foods.set(['italian'], 'gelato');
+foods.set(['mexican'], 'torta');
+foods.set(['canadian'], 'poutine');
 
 let southernUSStates = ['Tennessee', 'Kentucky', 'Texas'];
 foods.set(southernUSStates, 'hot chicken');
 
 console.log(
-  foods.get('italian'),
+  foods.get(['italian']),
   foods.get(southernUSStates),
   foods.size
 );
