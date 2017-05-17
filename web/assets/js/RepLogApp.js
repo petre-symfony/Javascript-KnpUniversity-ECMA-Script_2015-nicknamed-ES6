102c1e52964f22eeef30ae5f7708d6425bddf49e
petrero <petrero@gmail.com>
2017-05-17 21:25:01 +0300

    How can we fix helper property not being private? Here's an idea: above the class, create a new HelperInstance variable set to null. Then, instead of setting the new Helper onto a property - which is accessible from outside, say: HelperInstance = new Helper(). And that's it! The HelperInstance variable is not available outside our self-executing function. And of course down below, in updateTotalWeightLifted(), the code will now read: HelperInstance.getTotalWeightString(). And just like that, we've made Helper truly private.
    Multiple Instances with Map!
    
    Well... you might already see the problem! Even though we're not doing it here, it is legal to create multiple RepLogApp objects. And if we did create two RepLogApp objects, well the second would replace the HelperInstance from the first! We can only ever have one HelperInstance... even though we may have multiple RepLogApp objects. Bad design.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,10 +1,12 @@
 'use strict';
 
 (function(window, $, Routing, swal) {
+  let HelperInstance = null;
+  
   class RepLogApp {
     constructor($wrapper) {
       this.$wrapper = $wrapper;
-      this.helper = new Helper(this.$wrapper);
+      HelperInstance = new Helper(this.$wrapper);
 
       this.loadRepLogs();
 
@@ -43,7 +45,7 @@
 
     updateTotalWeightLifted() {
       this.$wrapper.find('.js-total-weight').html(
-        this.helper.getTotalWeightString()
+        HelperInstance.getTotalWeightString()
       );
     }
 
